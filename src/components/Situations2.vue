bbb4536ebfb3be1791a723e241c31aead31699b3
agonzal3 <adrigonzar97@gmail.com>
2021-04-13 22:08:10 +0200

    Abansos significatius en el about

---
+++ src/components/Situations2.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+        <!-- <img src=this.getimage> -->
+        <img v-if="counter == 0" :title=getimage() src="../assets/logo.png">
+        <!-- <p>{{getimage()}}</p> -->
+        <p>{{listdata[counter].text}}</p>
+
+    </div>
+</template>
+
+<script>
+//import { createCanvas, loadImage } from "canvas";
+
+
+
+export default {
+    name:'Concept1',
+    props: ['listdata','counter'],
+    methods:{
+        getimage(){
+            return this.listdata[this.counter].imagen
+            
+            //console.log(document.getElementById('mypic').setAttribute('src', xx))
+            //console.log(xx)
+
+            //return this.listdata[this.counter].imagen;
+        }
+
+        
+    }
+}
+</script>
