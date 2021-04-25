53a6e2d7177e931a5f6c9aa656ba2c9d84ca2e38
agonzal3 <adrigonzar97@gmail.com>
2021-04-25 23:28:59 +0200

    Intro y primera interación bien echa

--- src/components/Situations2.vue
+++ src/components/Situations2.vue
@@ -1,11 +1,36 @@
 <template>
     <div>
-
         <!-- <img src=this.getimage> -->
         <img v-if="counter == 0" :title=getimage() src="../assets/logo.png">
         <!-- <p>{{getimage()}}</p> -->
-        <p>{{listdata[counter].text}}</p>
-
+        <div v-if="counter < lista.length">
+            <p>{{lista[counter].text}}</p>
+        </div>
+        <div v-if="counter == lista.length">
+            <p>{{lista[lista.length-1].text}}</p>
+        </div>
+        <p>{{puntuation}}</p>
+        <b-container class="bv-example-row">
+            <div v-if="counter < lista.length">
+                <b-row align-h="between">
+                    <b-col cols="6"><b-button @click="correct">{{lista[counter].aswer1}}</b-button></b-col>
+                    <!-- <b-col cols="6"><b-button to='/concept/1' variant="outline-primary">{{lista[counter].aswer2}}</b-button></b-col> -->
+                    <b-col cols="6"><b-button @click="wrong">{{lista[counter].aswer2}}</b-button></b-col>
+                </b-row>
+            </div>
+            <div v-if="counter == lista.length">
+                <b-row align-h="between">
+                    <b-col cols="6"><b-button disabled>{{lista[lista.length-1].aswer1}}</b-button></b-col>
+                    <!-- <b-col cols="6"><b-button to='/concept/1' variant="outline-primary">{{lista[counter].aswer2}}</b-button></b-col> -->
+                    <b-col cols="6"><b-button disabled>{{lista[lista.length-1].aswer2}}</b-button></b-col>
+              </b-row>
+            </div>
+            
+        </b-container>
+        <p v-if="counter == lista.length && resoult > 2">De las anteriores situaciones has mostrado signos de FoMO en {{resoult}} de 4. {{respuestas[0].text}}. 
+            Las que estaban a la izquierda son las que eran signos de FoMO.</p>
+        <p v-if="counter == lista.length && resoult < 2">De las anteriores situaciones has mostrado signos de FoMO en {{resoult}} de 4. {{respuestas[1].text}}. 
+            Las que estaban a la izquierda son las que eran signos de FoMO.</p>
     </div>
 </template>
 
@@ -16,18 +41,74 @@
 
 export default {
     name:'Concept1',
-    props: ['listdata','counter'],
+    data(){
+        return {
+            lista:[
+              {text:'Tendría que dormir porque mañana voy al instituto, pero puedo escuchar que recibo algún mensaje y ...',
+               imagen:'../assets/logo.png', aswer1:'respondo el mensaje y dejo el móvil encendido.', aswer2:'apago el mobil de golpe.'},
+              {text:'Cuando me levanto de la cama lo primero que hago es ...',
+               imagen:'../assets/logo.png', aswer1:'coger el mobil para ver que se cuece en las redes sociales.', aswer2:'ir a desayunar o a mear.'},
+              {text:'He colgado una history o una publicación y ...',
+               imagen:'../assets/logo.png', aswer1:'estoy pendiente de los likes y comentarios que recibo.', aswer2:'no le doy importancia a las reaciones.'},
+              {text:'Estoy hablando con mis padres y recibo un mensaje, ...',
+               imagen:'../assets/logo.png', aswer1:'cojo el mobil para ver el mensaje.', aswer2:'me espero a terminar de hablar para responder.'}
+            ],
+            counter:0,
+            puntuation:0,
+            resoult:0,
+            respuestas:[
+                {text:'Muy bien!'},
+                {text:'jajaajjaaj'},
+            ]
+        }
+    },
+    //props: ['listdata','counter'],
     methods:{
         getimage(){
-            return this.listdata[this.counter].imagen
+            return this.lista[this.counter].imagen
             
             //console.log(document.getElementById('mypic').setAttribute('src', xx))
             //console.log(xx)
 
-            //return this.listdata[this.counter].imagen;
-        }
+            //return this.lista[this.counter].imagen;
+        },
+        counterb(){
+            if(this.counter<this.lista.length-1){
+                console.log(this.counter++);
+            }else{
+                this.counter=0;
+            }
 
+        },
+        correct(){
+            this.resoult +=1;
+            this.puntuation += 10;
+            this.counter++;
+        },
+        wrong(){
+            this.puntuation -= 10;
+            this.counter++;
+        }
         
+    },
+    created(){
+        console.log('Created')
+    },
+    mounted(){
+        console.log('Mounted')
+    },
+    updated(){
+        console.log(this.counter)
+        // if(this.counter == this.lista.length-1){
+        //     this.$destroy();
+        // }
+    },
+    beforeDestroy(){
+        console.log('beforeDestroy');
+    },
+    destroyed(){
+        console.log('destroy');
     }
+    
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="about">
-    <h1>Te ha passado alguna vez?</h1>
-    <!-- <Concept1 /> -->
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col cols="9">
+          <h1>Te ha passado alguna vez?</h1>
+          
+          <!-- <Concept1 /> -->
+          <Concept1 />
+          <!-- <b-container class="bv-example-row">
+            <b-row align-h="between">
+              <b-col cols="6"><b-button @click="home">M'he ha passado</b-button></b-col>
+              <b-col cols="6"><b-button to='/concept/1' variant="outline-primary">No m'he ha passado</b-button></b-col>
+            </b-row>
+          </b-container> -->
+        </b-col>
+      </b-row>
+    </b-container>
     
-
-      <Concept1 :listdata="lista" :counter="counter"/>
-
-      <b-container class="bv-example-row">
-        <b-row align-h="between">
-          <b-col cols="6"><b-button @click="home">M'he ha passado</b-button></b-col>
-          <b-col cols="6"><b-button to='/concept/1' variant="outline-primary">No m'he ha passado</b-button></b-col>
-        </b-row>
-      </b-container>
+ 
+    
       
   </div>
 </template>
@@ -23,26 +31,14 @@ export default {
     name: 'About',
     data(){
         return {
-            lista:[
-              {text:'En jordi es diu aixi',imagen:'../assets/logo.png'},
-              {text:'En pere es diu aixi',imagen:''},
-              {text:'En martin es diu aixi',imagen:''},
-              {text:'Leulalia es diu aixi',imagen:''}
-            ],
-            counter:0
+            
         }
     },
     components: {
         Concept1
     },
     methods:{
-        home(){
-          if(this.counter<this.lista.length-1){
-            console.log(this.counter++);
-          }else{
-            this.counter=0;
-          }
-        }
+        
     }
 
 }
@@ -50,3 +46,7 @@ export default {
 
 
 </script>
+
+<style scoped>
+
+</style>
